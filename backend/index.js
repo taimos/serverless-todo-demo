d2ca4b3b81a6e2fd74005af5e226a641d2b3390f
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2017-03-24 08:23:40 +0100

    use node6.10

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,4 @@
-require('strict-mode')(function () {
-  'use strict';
-  
-  exports.apiGetTodos = require('./api/getTodos').handler;
-  exports.apiAddTodo = require('./api/addTodo').handler;
-  exports.apiUpdateTodo = require('./api/updateTodo').handler;
-  
-});
+
+exports.apiGetTodos = require('./api/getTodos').handler;
+exports.apiAddTodo = require('./api/addTodo').handler;
+exports.apiUpdateTodo = require('./api/updateTodo').handler;
